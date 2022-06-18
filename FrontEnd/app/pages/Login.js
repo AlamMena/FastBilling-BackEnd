48f002da497ba5c8f672e574e1e19645fb00e855
Alex <alexmezsan@gmail.com>
2022-06-18 19:07:57 -0400

    added react hook form to the project

--- FrontEnd/app/pages/Login.js
+++ FrontEnd/app/pages/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import getAuth from "firebase/auth";
 
 export default function Login() {
+  const { register, handleSubmit } = useForm();
+
   return (
-    <div>
-      <span>Login</span>
+    <div className="login">
+      <form>
+        <div className="login__">Iniciar sesion</div>
+        <div className="input__username" type="text">
+          <input></input>
+        </div>
+        <div className="input__password" type="password">
+          <input></input>
+        </div>
+      </form>
     </div>
   );
 }
