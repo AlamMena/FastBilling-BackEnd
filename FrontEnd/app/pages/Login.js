3096afa66a778cf389d200a0ee61a74ed03d3d86
Alex <alexmezsan@gmail.com>
2022-06-19 16:27:32 -0400

    added a fix to firebase auth

--- FrontEnd/app/pages/Login.js
+++ FrontEnd/app/pages/Login.js
@@ -1,18 +1,38 @@
 import React from "react";
-import getAuth from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { app } from "../Firebase/FirebaseAppConfig";
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  const auth = getAuth(app);
+
+  signInWithEmailAndPassword(auth)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 
   return (
     <div className="login">
-      <form>
+      <p>{data}</p>
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
         <div className="login__">Iniciar sesion</div>
-        <div className="input__username" type="text">
-          <input></input>
+        <div className="input__useremail" type="text">
+          <input {...register("email")} placeholder="E-mail"></input>
         </div>
         <div className="input__password" type="password">
-          <input></input>
+          <input {...register("password")} placeholder="Contraseña"></input>
+        </div>
+        <div className="input__submit">
+          <input type="submit" />
         </div>
       </form>
     </div>
