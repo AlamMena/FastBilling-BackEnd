9e7d967a8efb44454d316a32df298c0776d20b8b
Alex <alexmezsan@gmail.com>
2022-06-19 16:57:00 -0400

    addeda working login

--- FrontEnd/app/pages/Login.js
+++ FrontEnd/app/pages/Login.js
@@ -9,21 +9,25 @@ export default function Login() {
   const [data, setData] = useState("");
   const auth = getAuth(app);
 
-  signInWithEmailAndPassword(auth)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+  const handleLogin = (data) => {
+    const { email, password } = data;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
 
   return (
     <div className="login">
       <p>{data}</p>
-      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+      <form onSubmit={handleSubmit((data) => handleLogin(data))}>
         <div className="login__">Iniciar sesion</div>
         <div className="input__useremail" type="text">
           <input {...register("email")} placeholder="E-mail"></input>
