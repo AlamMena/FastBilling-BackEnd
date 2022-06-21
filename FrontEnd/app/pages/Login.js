27861cab91d9e3bfe993ec3b175f0c168428f3b5
Alex <alexmezsan@gmail.com>
2022-06-21 19:38:35 -0400

    modified code and now works with async

--- FrontEnd/app/pages/Login.js
+++ FrontEnd/app/pages/Login.js
@@ -2,37 +2,48 @@ import React from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { app } from "../Firebase/FirebaseAppConfig";
+import { app, firebaseConfig } from "../Firebase/FirebaseAppConfig";
+import { async } from "@firebase/util";
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
   const [data, setData] = useState("");
   const auth = getAuth(app);
 
-  const handleLogin = (data) => {
-    const { email, password } = data;
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
+  let updateToken = async () => {
+    let response = await fetch();
+  };
+
+  const handleLogin = async (data) => {
+    try {
+      console.log(data);
+      const { email, password } = data;
+      const response = signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {}
+
+    // .then((userCredential) => {
+    //   // Signed in
+    //   const user = userCredential.user;
+    //   // ...
+    // })
+    // .catch((error) => {
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   console.log(errorMessage);
+    // });
   };
 
   return (
     <div className="login">
       <p>{data}</p>
+      {/* Log In user form  */}
       <form onSubmit={handleSubmit((data) => handleLogin(data))}>
         <div className="login__">Iniciar sesion</div>
-        <div className="input__useremail" type="text">
+        <div className="input__useremail" type="text" name="email">
           <input {...register("email")} placeholder="E-mail"></input>
         </div>
-        <div className="input__password" type="password">
+        <div className="input__password" type="password" name="password">
           <input {...register("password")} placeholder="Contraseña"></input>
         </div>
         <div className="input__submit">
