593d248f5d104fd9fc5f8e048dfc0da8984741e4
Alex <alexmezsan@gmail.com>
2022-06-24 18:19:55 -0400

    a little styling added to dashboard

--- FrontEnd/app/Components/DashboardMenuComponents/Menu.js
+++ FrontEnd/app/Components/DashboardMenuComponents/Menu.js
@@ -1,25 +1,51 @@
 import React from "react";
 import { useState } from "react";
 import styles from "../DashboardMenuComponents/dashboard.module.css";
-import { AiOutlineMenu, AiOutlineCloseCircle } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineCloseCircle,
+  AiFillSetting,
+  AiOutlineFundView,
+  AiFillPieChart,
+  AiFillHome,
+} from "react-icons/ai";
 
 export default function Menu() {
   const [openMenu, setOpenMenu] = useState();
 
   const hamburberIcon = <AiOutlineMenu onClick={() => setOpenMenu(true)} />;
   const closeIcon = <AiOutlineCloseCircle onClick={() => setOpenMenu(false)} />;
+  const elements = (
+    <div className={styles.dashboard_elements}>
+      <div className="flex flex-row space-x-2 items-center">
+        <AiFillHome />
+        <p>Dashboard</p>
+      </div>
+      <div className="flex flex-row space-x-2 items-center">
+        <AiFillPieChart />
+        <p>Estadisticas</p>
+      </div>
+      <div className="flex flex-row space-x-2 items-center">
+        <AiOutlineFundView />
+        <p>Vision general</p>
+      </div>
+      <div className="flex flex-row space-x-2 items-center">
+        <AiFillSetting />
+        <p>Ajustes</p>
+      </div>
+    </div>
+  );
 
   return (
-    <div className={styles.dashboard_menu}>
+    <div className={styles.dashboard}>
+      <div className={styles.dashboard_menu}>
+        <img src="https://hub.fastbilling.app/uploads/-/system/appearance/logo/1/LOGO-SISTEMA-FACTURACION-2.png" />
+        {elements}
+      </div>
       <div className={styles.dashboard_burger}>
         {openMenu ? closeIcon : hamburberIcon}
       </div>
-      <div className={styles.dashboard_elements}>
-        <p>Dashboard</p>
-        <p>Estadisticas</p>
-        <p>Vision general</p>
-        <p>Ajustes</p>
-      </div>
+      {openMenu ? elements : ""}
     </div>
   );
 }
--- FrontEnd/app/pages/Dashboard.js
+++ FrontEnd/app/pages/Dashboard.js
@@ -4,10 +4,10 @@ import Menu from "../Components/DashboardMenuComponents/Menu";
 export default function Dashboard() {
   return (
     <div className=" grid grid-flow-col ">
-      <div className=" col-span-1 ">
+      <div className="col-span-1">
         <Menu />
       </div>
-      <div className="col-span-10">
+      <div className="col-span-11">
         <p>Component 2</p>
       </div>
     </div>
