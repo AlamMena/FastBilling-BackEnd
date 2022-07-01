ab6328da799b13a7bc4fa695d8aef913b1a3e34d
Alam <amenabeato@gmail.com>
2022-06-30 20:10:56 -0400

    authcontext added

--- FrontEnd/app/pages/_app.js
+++ FrontEnd/app/pages/_app.js
@@ -16,10 +16,10 @@ function MyApp({ Component, pageProps }) {
       <PrivateRoute>
         <div className="flex flex-col">
           {/* Side Bar grid */}
-          <div className=" h-20 absolute md:h-screen ">{/* <SideBar /> */}</div>
+          <div className=" h-20 absolute md:h-screen "><SideBar /></div>
           <div className="flex flex-col">
             {/* Top Bar grid  */}
-            <div className="fixed">{/* <TopBar /> */}</div>
+            <div className="fixed"><TopBar /></div>
             <div className=" md:ml-20 lg:ml-44 grid grid-cols-12 mt-20">
               {/* Content grid */}
               <div className="ml-3 col-span-12 md:col-span-10 lg:col-span-7">
